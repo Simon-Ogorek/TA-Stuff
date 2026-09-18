49bd7de6920315e78976868110668925dfb53ce7
Simon <ogoreksimon@gmail.com>
2026-09-17 23:59:30 -0400

    Fixes

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -5,7 +5,7 @@ public class Rotator : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-     transform.Rotate(new Vector3 (15, 30, 45) * Time.deltaTime);   
+    transform.Rotate(new Vector3 (15, 30, 45) * Time.deltaTime);   
     }
 
     // Update is called once per frame
